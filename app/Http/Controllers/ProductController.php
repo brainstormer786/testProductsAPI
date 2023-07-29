e00a5ac92784a648ff6f508c7bdb44323700c88d
bilal <brainstormer786@gmail.com>
2023-07-30 04:02:46 +0500

    initial commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,8 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::all();
-        return $response()->json($products);
+        $products = Product::paginate();
+        return response()->json($products);
     }
 
     /**
@@ -26,13 +26,18 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'quantity' => 'required|integer'
+        ]);
+
         $product = new Product();
         $product->name = $request->get('name');
         $product->description = $request->get('description') ?? null;
         $product->quantity = $request->get('quantity');
         $product->save();
 
-        return $response()->json(["message" => "Product added successfully!"]);
+        return response()->json(["message" => "Product added successfully!"]);
     }
 
     /**
@@ -45,13 +50,13 @@ class ProductController extends Controller
     {
         $product = Product::find($id);
         if($product)
-            return $response()->json($product);
+            return response()->json($product);
         else
-            return $response()->json(["error" => "Product did not found."], 404);
+            return response()->json(["error" => "Product did not found."], 404);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified product in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
@@ -59,17 +64,59 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $product = Product::find($id);
-        $product->name = $request->get('name');
-        $product->description = $request->get('description') ?? null;
-        $product->quantity = $request->get('quantity');
-        $product->save();
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'quantity' => 'required|integer'
+        ]);
 
-        return $response()->json(["message" => "Product updated successfully!"]);
+        $product = Product::find($id);
+        if($product) {
+            if($request->has('quantity'))
+                $product->quantity = $request->get('quantity');
+            
+            if($product->update($request->all()))
+                return response()->json(["message" => "Product updated successfully!"]);
+            else
+                return response()->json(["error" => "Failed to update product."]);
+        } else {
+                return response()->json(["error" => "Product not found."], 404);
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Add/update product in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function addImage(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'image' => 'required|image'
+        ]);
+        
+        $product = Product::find($id);
+        if($request->hasFile('image')) {
+            $file = $request->file('image');
+            $imageName = time().'.'.$file->extension();
+            $imagePath = public_path(). '/files';
+
+            $file->move($imagePath, $imageName);
+            $product->image = '/files/'.$imageName;
+            $product->save();
+
+            return response()->json([
+                "success" => true,
+                "message" => "Image has been uploaded successfully."
+            ]);    
+        }
+
+        return response()->json(["error" => "Product not found to attach image."], 404);
+    }
+
+    /**
+     * Remove the specified product from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -79,9 +126,9 @@ class ProductController extends Controller
         $product = Product::find($id);
         if($product) {
             $product->delete();
-            return $response()->json(["message" => "Product deleted successfully!"]);
+            return response()->json(["message" => "Product deleted successfully!"]);
         } else {
-            return $response()->json(["error" => "Product not found"], 404);
+            return response()->json(["error" => "Product not found"], 404);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,13 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::resource('products', ProductController::class);
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/products/add_image/{id}', [ProductController::class, 'addImage']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
